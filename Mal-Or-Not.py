cfa5b4237beffe7f13529c5f0f38fbc973b585c6
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-12-06 01:53:35 +0530

    Update Mal-Or-Not.py

--- Mal-Or-Not.py
+++ Mal-Or-Not.py
@@ -88,8 +88,43 @@ def phno():
 
 def link():
 	global typeid
-	typeid='link'
-	clickentry()
+	typeid='url'
+	rootentry = Tk()
+	rootentry.title('URL Intel')
+	rootentry.geometry("350x200+670+300")
+	#bg= PhotoImage(file="matrixbg.png")
+	url_canvas = Canvas(rootentry, width=200, height=100, bd=0, highlightthickness=0, bg="green")
+	url_canvas.pack(fill="both", expand=True)
+
+	#my_canvas.create_image(0,0, image=bg, anchor="nw")
+	url_canvas.create_text(180,45, text="Enter URL:", font=("Helvetica", 18,'bold'), fill="white")
+	entry = Entry(rootentry, font=("Helvitica",12),width=13, fg="black", bd=0)
+	entry_window = url_canvas.create_window(115,80,anchor='nw', window=entry)
+	def store():
+	    global inp
+	    inp=entry.get()
+	    print(typeid+":"+inp)
+	    rootentry.destroy()
+	    
+	    subprocess.check_output(["./urlreport.sh", inp])
+	    
+	    rooturl = Tk()
+	    rooturl.title('URL Information')
+	    rooturl.geometry("850x500+670+300")
+	    
+	    my_canvas=Canvas(rooturl, bg='black', bd=0, highlightthickness=0, relief='ridge')
+	    my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+	    
+	    with open(inp.split("/")[2]+".report","r") as f:
+	    	data=f.read()
+	    text_key= Label(my_canvas, text=data, anchor="w", justify=LEFT, font='"Helvetica" 12', bg='black', fg='lime').grid(row=0, column=0)
+
+	    rooturl.mainloop()
+
+	buttonentry=Button(rootentry, text="Done",font=("times",12),width=5,padx=15, pady=7, fg='white', bg='black', bd=0, command=store)
+	buttonentry_window = url_canvas.create_window(145,130, anchor='nw', window=buttonentry)
+
+	rootentry.mainloop()
 
 def files():
 	global typeid
