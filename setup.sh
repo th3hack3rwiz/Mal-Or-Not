90e0ecd3907e657c161423518823f259461ced3e
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-12-06 00:59:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,11 @@ pip install futures
 pip install PySocks
 sudo apt-get install figlet
 chmod +x mal-o-not.sh
+read -p "Enter ip2location API key:" ip2location
+read -p "Enter ipqualityscore API key:" ipquality
+read -p "Enter virustotal API key:" virustotal
+sed -i "s#XXXX#$virustotal#" urlreport.sh
+sed -i "s#YYYY#$ip2location#" ipintel.sh
+sed -i "s#ZZZZ#$ipquality#" ipintel.sh
+chmod +x ipintel.sh
+chmod +x urlreport.sh
