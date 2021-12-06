8df2b6e5a74f1c790839e8c146943032d1ce4626
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-12-06 18:15:35 +0530

    Update number.sh

--- number.sh
+++ number.sh
@@ -1,2 +1,2 @@
 number=$1
-curl "http://apilayer.net/api/validate?access_key=4ee6f2b262e3d6c7eb66a54b517b891a&number=$number&format=1" | grep -E "valid|local_format|international_format|country_prefix|country_code|country_name|location|carrier|line_type" | tr -d "\"," | sed 's/:/ : /g' | tee  $number.report
+curl "http://apilayer.net/api/validate?access_key=NNNN&number=$number&format=1" | grep -E "valid|local_format|international_format|country_prefix|country_code|country_name|location|carrier|line_type" | tr -d "\"," | sed 's/:/ : /g' | tee  $number.report
