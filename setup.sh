c01bdb84568dc693dc6baed261ca818293c73c2c
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-12-06 18:12:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,10 @@ chmod +x mal-o-not.sh
 read -p "Enter ip2location API key:" ip2location
 read -p "Enter ipqualityscore API key:" ipquality
 read -p "Enter virustotal API key:" virustotal
+read -p "Enter numverify API key:" numverify
 sed -i "s#XXXX#$virustotal#" urlreport.sh
 sed -i "s#YYYY#$ip2location#" ipintel.sh
 sed -i "s#ZZZZ#$ipquality#" ipintel.sh
+sed -i "s#NNNN#$numverify#" number.sh
 chmod +x ipintel.sh
 chmod +x urlreport.sh
