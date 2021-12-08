1cdaa93c5fa5118b156bed88aaf3166da1862313
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-12-08 17:03:11 +0530

    Update number.sh

--- number.sh
+++ number.sh
@@ -2,3 +2,4 @@ number=$1
 curl "http://apilayer.net/api/validate?access_key=NNNN&number=$number&format=1" | grep -E "valid|local_format|international_format|country_prefix|country_code|country_name|location|carrier|line_type" | tr -d "\"," | sed 's/:/ : /g' > $number.number.report
 echo -e "Number: $1\n" >> number.report
 cat $number.report >> number.report
+echo -e "\n" >> number.report
