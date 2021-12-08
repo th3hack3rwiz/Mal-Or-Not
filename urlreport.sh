71b5864ee7c6c1b7ea83cc7450bce8b2c5ca9040
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-12-08 16:54:25 +0530

    Update urlreport.sh

--- urlreport.sh
+++ urlreport.sh
@@ -4,8 +4,8 @@ curl --request POST --url https://www.virustotal.com/api/v3/urls --header 'x-api
 id=$(cat result1 | jq | grep id | awk -F "\"" '{print $4}')
 rm result1
 curl --request GET --url https://www.virustotal.com/api/v3/analyses/$id --header 'x-apikey: XXXX' > result2
-echo -e "\nAccording to VirusTotal API:\n"  | tee $name.report
-cat result2 | jq | grep -E "harmless|malicious|suspicious|undetected" | grep -iEv "Result|category" | tr -d "\"" | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' | tee -a $name.report
+echo -e "\nAccording to VirusTotal API:\n"  | tee $name.url.report
+cat result2 | jq | grep -E "harmless|malicious|suspicious|undetected" | grep -iEv "Result|category" | tr -d "\"" | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' | tee -a $name.url.report
 #phishing_count=$(cat result2 | grep phishing | grep result | wc -l)
 #malware_count=$(cat result2 | grep malware | grep result | wc -l)
 #echo -e "\tMalware: $malware_count"
@@ -15,10 +15,12 @@ if [ -z "$malicious_sources" ];
 then
 	echo lol >/dev/null
 else
-	echo -e "\nResult:" | tee -a $name.report
+	echo -e "\nResult:" | tee -a $name.url.report
 	for i in $(echo $malicious_sources)
 	do
-		echo -e "\t$i - $(cat result2| grep $i -A 2  | grep result | tr -d '\",' | awk '{print $2}')" | tee -a $name.report 
+		echo -e "\t$i - $(cat result2| grep $i -A 2  | grep result | tr -d '\",' | awk '{print $2}')" | tee -a $name.url.report 
 	done
 fi
+echo -e "URL: $1\n" >> url.report
+cat $name.url.report >> url.report
 rm result2
