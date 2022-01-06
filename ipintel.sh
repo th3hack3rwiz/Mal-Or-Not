c113bc83f3eb09299cdf46185b134dba939b97ef
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2022-01-07 04:00:29 +0530

    Update ipintel.sh

--- ipintel.sh
+++ ipintel.sh
@@ -4,18 +4,17 @@ function gatherIPIntel(){
 		  	wget "https://api.ip2location.com/v2/?key=YYYY&ip=$1&package=WS24&format=json&addon=continent,country,region,city,geotargeting,country_groupings,time_zone_info&lang=en" --wait=3 -U 'Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' --no-http-keep-alive --no-check-certificate --tries=1 -O test4.html > /dev/null 2>&1
 		  	cat test4.html | anew apiGeo-Location > geo
 		  	wget "https://ipqualityscore.com/api/json/ip/ZZZZ/$1?strictness=0&allow_public_access_points=true&fast=true&lighter_penalties=true&mobile=true" --wait=3 -U 'Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' --no-http-keep-alive --no-check-certificate --tries=1 -O log > /dev/null 2>&1
-		  	echo "  IP Address: $1" > $1.ip.report
-		  	cat log | jq | grep -E "proxy|vpn|tor|fraud" | grep -v "active" | tr -d '\"' | sed 's/,//g' | sed 's/_/ /g' | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' >> $1.ip.report
-		  	cat geo | jq | grep -E '"country_name":|"city_name":|"zip_code":|"time_zone":|"isp":|"domain":|"region_name"|"latitude"|"longitude"' | sed 's/,//g'  | sed 's/"//g' | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' | sed 's/_/ /g' >> $1.ip.report
+		  	echo "  IP Address: $1\n\nResult:\n" > output/ip/$1.ip.report
+		  	cat log | jq | grep -E "proxy|vpn|tor|fraud" | grep -v "active" | tr -d '\"' | sed 's/,//g' | sed 's/_/ /g' | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' >> > output/ip/$1.ip.report
+		  	cat geo | jq | grep -E '"country_name":|"city_name":|"zip_code":|"time_zone":|"isp":|"domain":|"region_name"|"latitude"|"longitude"' | sed 's/,//g'  | sed 's/"//g' | sed 's/\<\([[:lower:]]\)\([[:alnum:]]*\)/\u\1\2/g' | sed 's/_/ /g' >> > output/ip/$1.ip.report
 		  	intel=$(cat geo | jq | grep -E '"country_name":|"city_name":|"zip_code":|"time_zone":|"isp":|"domain":|"region_name"|"latitude"|"longitude"' | sed 's/,//g'  | sed 's/"//g' | awk -F ": " '{print $2}' | sed 's/[[:space:]]/_/g' | xargs)
 		  lat=$(echo $intel | awk '{print $4}')
 		  long=$(echo $intel | awk '{print $5}')
 		  geoURL="https://www.gps-coordinates.net/latitude-longitude/$lat/$long/10/roadmap"
-		    echo -e "  Geographical Location: $geoURL" >> $1.ip.report
+		    echo -e "  Geographical Location: $geoURL" >> output/ip/$1.ip.report
 		    time=$(timedatectl | head -n 1 | awk -F ":" '{print $2":"$3":"$4}') 
-		    echo "  Intel gathered at:$time" >> $1.ip.report
-			echo -e "IP Address: $1\n" >> ip.report
-			cat $1.ip.report >> ip.report
+		    echo "  Intel gathered at:$time" >> output/ip/$1.ip.report
+		    cat output/ip/$1.ip.report >> output/ip/ip.master.report
 rm apiGeo-Location geo test4.html log
 }
 
