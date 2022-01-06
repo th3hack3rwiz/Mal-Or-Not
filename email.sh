420429f2f511028652f5b692f2315f6c238c8a78
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2022-01-07 00:38:03 +0530

    Update email.sh

--- email.sh
+++ email.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 email=$1
 filename=$(echo $1 | awk -F "@" '{print $1}')
-curl -s https://ipqualityscore.com/api/json/email/52euadgGvFpxYkflxorqnBTwGY8mEwMi/$1 | jq | grep -E "valid|disposable|smtp_score|dns_valid|deliverability|recent_abuse|fraud_score|leaked|honeypot"  | tr -d "\"," > /output/email/$filename.email.report
-echo -e "Email: $1\n" >> /output/email/email.report
-cat $filename.email.report >> /output/email/email.report
+echo -e "E-mail Address: $email\n\nResult:">output/email/$filename.email.report
+curl -s https://ipqualityscore.com/api/json/email/52euadgGvFpxYkflxorqnBTwGY8mEwMi/$1 | jq | grep -E "valid|disposable|smtp_score|dns_valid|deliverability|recent_abuse|fraud_score|leaked|honeypot"  | tr -d "\"," >> output/email/$filename.email.report
+cat output/email/$filename.email.report >> output/email/email.master.report
+echo -e "\n" >> output/email/email.master.report
